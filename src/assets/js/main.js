43b05c6ad4dcd8a0c7dfee51645810be400a6285
Akshay Dhiman <akshay.virgo@gmail.com>
2020-01-27 23:31:16 +0530

    add hero carousel

---
+++ src/assets/js/main.js
@@ -0,0 +1,18 @@
+var heroBackground = document.getElementsByClassName('index-hero__background')[0];
+var heroBackgroundChildren = heroBackground.children;
+var state = 0;
+
+setInterval(function() {
+  for (var i = 0; i < heroBackgroundChildren.length; i++) {
+    if (state === i) {
+      heroBackgroundChildren[i].style.opacity = 1;
+    } else {
+      heroBackgroundChildren[i].style.opacity = 0;
+    }
+  }
+  if (state === 5) {
+    state = 0;
+  } else {
+    state++;
+  }
+}, 4000);
