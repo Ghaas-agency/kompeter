fd832d9d22e7e201f8469da7951c04e6b7bcae28
Akshay Dhiman <akshay.virgo@gmail.com>
2020-01-29 15:21:40 +0530

    add footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,20 @@
-var heroBackground = document.getElementsByClassName('index-hero__background')[0];
-var heroBackgroundChildren = heroBackground.children;
-var state = 0;
-
-setInterval(function() {
-  for (var i = 0; i < heroBackgroundChildren.length; i++) {
-    if (state === i) {
-      heroBackgroundChildren[i].style.opacity = 1;
-    } else {
-      heroBackgroundChildren[i].style.opacity = 0;
+(function() {
+  var heroBackground = document.getElementsByClassName('index-hero__background')[0];
+  var heroBackgroundChildren = heroBackground.children;
+  var state = 0;
+  
+  setInterval(function() {
+    for (var i = 0; i < heroBackgroundChildren.length; i++) {
+      if (state === i) {
+        heroBackgroundChildren[i].style.opacity = 1;
+      } else {
+        heroBackgroundChildren[i].style.opacity = 0;
+      }
     }
-  }
-  if (state === 5) {
-    state = 0;
-  } else {
-    state++;
-  }
-}, 4000);
+    if (state === 5) {
+      state = 0;
+    } else {
+      state++;
+    }
+  }, 4000);
+})();
